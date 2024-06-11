7231d64ee01323068efd409af5a17836ef231528
Marcelo <76289550+portobanco51@users.noreply.github.com>
2024-06-11 01:11:22 -0600

    Fix: updated indentation and EOL for unix-based distributions

--- menu.sh
+++ menu.sh
@@ -2,79 +2,77 @@
 
 function MENU() {
 
-  # COLORS
-  GRAY="$(tput setaf 0; tput bold)"
-  RED="$(tput setaf 1; tput bold)"
-  GREEN="$(tput setaf 2; tput bold)"
-  YELLOW="$(tput setaf 3; tput bold; tput smul)"
-  BLUE="$(tput setaf 4; tput bold)"
-  MAGENTA="$(tput setaf 5; tput bold)"
-  CYAN="$(tput setaf 6; tput bold)"
-  WHITE="$(tput setaf 7; tput bold)"
-  EOS="$(tput sgr0)"
+	# COLORS
+	GRAY="$(tput setaf 0; tput bold)"
+	RED="$(tput setaf 1; tput bold)"
+	GREEN="$(tput setaf 2; tput bold)"
+	YELLOW="$(tput setaf 3; tput bold; tput smul)"
+	BLUE="$(tput setaf 4; tput bold)"
+	MAGENTA="$(tput setaf 5; tput bold)"
+	CYAN="$(tput setaf 6; tput bold)"
+	WHITE="$(tput setaf 7; tput bold)"
+	EOS="$(tput sgr0)"	
+	# MENU
+	PROMPT=("$1")
+	DIV="===================="
+	ARROW="$(tput setaf 3; tput bold)"
+	INDICATOR="-->"
+	SELECTED=0
+	OPTIONS="${@:2}"
+	LENGTH="$((${#OPTIONS[@]} - 4))"
 
-  # MENU
-  PROMPT=("$1")
-  DIV="===================="
-  ARROW="$(tput setaf 3; tput bold)"
-  INDICATOR="-->"
-  SELECTED=0
-  OPTIONS="${@:2}"
-  LENGTH="$((${#OPTIONS[@]} - 4))"
-  
-  # FUNCTIONS
-  PRINT_MENU() {
-    # Runs clear to prevent infinite scroll when choosing
-    clear
-    # Displays menu header
-    echo -e "$GREEN$PROMPT$EOS"
-    echo -e "$GREEN$DIV$EOS"
-    # Displays menu options
-    for (( i=0;i<${#OPTIONS[@]};i++ ))
-    do
-      if [[ $SELECTED -eq $i ]]
-      # Renders current option in bold
-      then
-        OPT=${OPTIONS[$i]}
-        echo -e "\n  $ARROW$INDICATOR$EOS $YELLOW$OPT$EOS"
-      # Renders other options
-      else
-        OPT=${OPTIONS[$i]}
-        echo -e "\n$GRAY$OPT$EOS"
-      fi
-    done
-    # Displays menu footer
-    echo -e "\n$GREEN$DIV$EOS"
-  }
+	# FUNCTIONS
+	PRINT_MENU() {
+		# Runs clear to prevent infinite scroll when choosing
+		clear
+		# Displays menu header
+		echo -e "$GREEN$PROMPT$EOS"
+		echo -e "$GREEN$DIV$EOS"
+		# Displays menu options
+		for (( i=0;i<${#OPTIONS[@]};i++ ))
+		do
+			if [[ $SELECTED -eq $i ]]
+			# Renders current option in bold
+			then
+				OPT=${OPTIONS[$i]}
+				echo -e "\n  $ARROW$INDICATOR$EOS $YELLOW$OPT$EOS"
+			# Renders other options
+			else
+				OPT=${OPTIONS[$i]}
+				echo -e "\n$GRAY$OPT$EOS"
+			fi
+		done
+		# Displays menu footer
+		echo -e "\n$GREEN$DIV$EOS"
+	}
 
-  PRINT_MENU
-
-  # Reads user input // Navigation
-  while read -rsn1 input
-  do
+	PRINT_MENU	
+	# Reads user input // Navigation
+	while read -rsn1 input
+	do
     case $input in
-      "A")
-        if [[ $SELECTED -lt 1 ]]
-        then
-          SELECTED=$(($LENGTH + 3))
-        else
-          SELECTED=$(($SELECTED - 1))
-        fi
-        PRINT_MENU
-       ;;
-      "B")
-        if [[ $SELECTED -gt $(($LENGTH + 2)) ]]
-        then
-          SELECTED=0
-        else
-          SELECTED=$(($SELECTED + 1))
-        fi
-        PRINT_MENU
-       ;;
-      # Returns selected option
-      "") return $(($SELECTED)) ;;
+		"A")
+			if [[ $SELECTED -lt 1 ]]
+			then
+				SELECTED=$(($LENGTH + 3))
+			else
+				SELECTED=$(($SELECTED - 1))
+			fi
+			PRINT_MENU
+		;;
+		"B")
+			if [[ $SELECTED -gt $(($LENGTH + 2)) ]]
+			then
+				SELECTED=0
+			else
+				SELECTED=$(($SELECTED + 1))
+			fi
+			PRINT_MENU
+		;;
+		# Returns selected option
+		"") return $(($SELECTED)) ;;
     esac
-  done
+	done
 }
 
 #################################################################################################################
